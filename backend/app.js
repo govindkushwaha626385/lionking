8bafb38270f171a4e6d6572c39ce2167731ee723
Govind Singh Kushwaha <govindkushwaha6263@gmail.com>
2024-10-06 17:57:15 +0530

    remove withcrediantial : true from backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const pool = require("./db/database");
 app.use(
   cors({
     origin: "https://lionking-frontend.vercel.app",
-    credentials: true,
+    // credentials: true,
   })
 );
 // const corsOptions = {
