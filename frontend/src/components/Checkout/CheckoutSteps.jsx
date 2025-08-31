3babc6995c36b8404ac19f1e96e8e95824bb8268
GOVIND SINGH KUSHWAHA <127130035+govindkushwaha626385@users.noreply.github.com>
2025-08-31 18:20:41 +0530

    Update CheckoutSteps.jsx

--- frontend/src/components/Checkout/CheckoutSteps.jsx
+++ frontend/src/components/Checkout/CheckoutSteps.jsx
@@ -6,15 +6,6 @@ const CheckoutSteps = ({active}) => {
   return (
     <div className='w-full flex justify-center'> 
         <div className="w-[90%] 800px:w-[50%] flex items-center flex-wrap">
-               {/* <div className={`${styles.noramlFlex}`}>
-                <div className={`${styles.cart_button}`}>
-                       <span className={`${styles.cart_button_text}`}>1.Shipping</span>
-                </div>
-                <div className={`${
-                    active > 1 ? "w-[30px] 800px:w-[70px] h-[4px] !bg-[#f63b60]"
-                    : "w-[30px] 800px:w-[70px] h-[4px] !bg-[#FDE1E6]"
-                }`} />
-               </div> */}
 
                <div className={`${styles.noramlFlex}`}>
                 <div className={`${active > 1 ? `${styles.cart_button}` : `${styles.cart_button} !bg-[#FDE1E6]`}`}>
